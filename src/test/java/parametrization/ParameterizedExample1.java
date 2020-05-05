83a59a8fb07009b204fa3e38d1b083606942e49e
1taras <taras.honcharenko@gmail.com>
2020-05-05 17:39:21 +0300

    Add basic parametized test examples and xml TestNG runner

--- src/test/java/parametrization/ParameterizedExample1.java
+++ src/test/java/parametrization/ParameterizedExample1.java
@@ -3,14 +3,15 @@ package parametrization;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class ParametrizedExample2 {
+public class ParameterizedExample1 {
     @Test
-    @Parameters({"login", "password"})
-    public void paramtrized1(String login, String password) {
-        System.out.println("[ Parametrized test 2]");
+    @Parameters({"login","password"})
+    public void paramtrized1(String login, String password){
+        System.out.println("[ Parametrized test 1]");
         System.out.println("Login :    " + login);
         System.out.println("Password : " + password);
         System.out.println("[<-                ->]");
+
     }
 
 }
--- src/test/java/parametrization/ParameterizedExample2.java
+++ src/test/java/parametrization/ParameterizedExample2.java
@@ -3,15 +3,14 @@ package parametrization;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class ParametrizedExample1 {
+public class ParameterizedExample2 {
     @Test
-    @Parameters({"login","password"})
-    public void paramtrized1(String login, String password){
-        System.out.println("[ Parametrized test 1]");
+    @Parameters({"login", "password"})
+    public void paramtrized1(String login, String password) {
+        System.out.println("[ Parametrized test 2]");
         System.out.println("Login :    " + login);
         System.out.println("Password : " + password);
         System.out.println("[<-                ->]");
-
     }
 
 }
